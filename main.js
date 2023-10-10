fb33212d5391bf5faae1e3a4ad6241d63cc8024d
대현 <rkdeown10@naver.com>
2023-10-11 04:03:39 +0900

    인프런 강의 배속 기능 추가 10배속

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function test() {
   if (a.length == 1) {
       a[0].click();
   }
+  document.getElementsByClassName('playback-rate__slider css-1yxwq3l')[0].setAttribute("max",1000);
 }
 
 setInterval(() => test(), 3000);
