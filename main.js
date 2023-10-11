654992d1d42a0bc6178de8ce2de6012771157b98
대현 <rkdeown10@naver.com>
2023-10-11 16:43:44 +0900

    자동재생 추가, 배속 삭제

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 function test() {
   var a = document.getElementsByClassName('mantine-UnstyledButton-root mantine-Button-root css-x644ap');
+  
   if (a.length == 1) {
       a[0].click();
   }
-  document.getElementsByClassName('playback-rate__slider css-1yxwq3l')[0].setAttribute("max",1000);
+
+  document.querySelector('#player-container > div.css-2pste9.mantine-1avyp1d > div > div.shaka-controls-container').setAttribute("shown","true")
+  
+  if (document.querySelector('#play-pause-button-ui > div > button > svg').getAttribute('data-icon') == 'play'){
+    document.querySelector('#play-pause-button-ui > div > button').click();
+  }
+  
 }
 
 setInterval(() => test(), 3000);
