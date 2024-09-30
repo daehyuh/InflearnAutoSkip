84d9d438d5efe700d09ab0f16408fbae7e86e912
leestana01 <leestana01@naver.com>
2024-09-30 14:17:25 +0900

    feat: issue-#1 강의 자동 변경 및 일부 버그 수정, 주석 추가

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 function test() {
+  // 다음 강의 있는지 확인 -> 강의 끝나면 더 이상 진행 안되도록 하기 위함
+  var nextButton = document.querySelector("#root > main > div > footer > div > div > button.mantine-UnstyledButton-root.mantine-Button-root.mantine-c5w2vi");
+  if (!nextButton){
+    return;
+  }
+  skip();
+}
+
+function skip() {
+  // 다음 강의 수강하기 버튼 클릭
   var a = document.getElementsByClassName('mantine-UnstyledButton-root mantine-Button-root css-x644ap');
   
   if (a.length == 1) {
       a[0].click();
   }
 
-  document.querySelector('#player-container > div.css-2pste9.mantine-1avyp1d > div > div.shaka-controls-container').setAttribute("shown","true")
-  
-  if (document.querySelector('#play-pause-button-ui > div > button > svg').getAttribute('data-icon') == 'play'){
+  // 재생 바 보이도록 설정
+  var controlsContainer = document.querySelector('#player-container > div.css-2pste9.mantine-1avyp1d > div > div.shaka-controls-container');
+  if (controlsContainer) {
+    controlsContainer.setAttribute("shown","true");
+  }
+
+  // Play 버튼으로 실행 여부 판단 -> 강제 실행
+  var playButtonIcon = document.querySelector('#play-pause-button-ui > div > button > svg');
+  if (playButtonIcon && playButtonIcon.getAttribute('data-icon') == 'play') {
     document.querySelector('#play-pause-button-ui > div > button').click();
   }
-  
+
+  // 끝으로 이동
+  var video = document.querySelector('#player-container video');  
+  if (video) {
+    video.currentTime = video.duration; // 동영상을 끝으로 이동
+  }
 }
 
-setInterval(() => test(), 3000);
+const interval = setInterval(() => test(), 2000);
